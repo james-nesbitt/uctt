13f200e088ef481c061c9591483241dddaac8d7b
James Nesbitt <jnesbitt@mirantis.com>
2021-02-07 13:41:35 -0500

    constructors now base on config over dict
    - also bump version and configerus version bumping

--- uctt/constructors.py
+++ uctt/constructors.py
@@ -7,6 +7,7 @@ import logging
 from typing import List, Dict, Any
 
 from configerus.config import Config
+from configerus.loaded import LOADED_KEY_ROOT
 
 from .plugin import Factory, Type, UCTTPlugin
 from .instances import PluginInstances
@@ -66,43 +67,6 @@ def new_plugins_from_config(
     return new_plugins_from_dict(
         config=config, type=type, plugin_list=plugin_list)
 
-
-def new_plugin_from_config(config: Config, type: Type,
-                           label: str, key: str) -> UCTTPlugin:
-    """ Create a plugin from some config
-
-    This method will interpret some config values as being usable to build plugin
-
-    Parameters:
-    -----------
-
-    config (Config) : Used to load and get the plugin configuration
-
-    type (.plugin.Type) : plugin type to create
-
-    label (str) : config label to load to pull plugin configuration. That
-        label is loaded and config is pulled to produce a list of plugins
-
-    key (str) : config key to get a Dict of plugins configurations.
-
-    Returns:
-    --------
-
-    PluginInstances of your type
-
-    Raises:
-    -------
-
-    If you ask for a plugin which has not been registered, then you're going to
-    get a NotImplementedError exception.
-    To make sure that your desired plugin is registered, make sure to import
-    the module that contains the factory method with a decorator.
-
-    """
-    config_plugin = config.load(label)
-    plugin_dict = config_plugin.get(key, exception_if_missing=True)
-    return new_plugin_from_dict(
-        config=config, type=type, plugin_dict=plugin_dict)
 
 
 def new_plugins_from_dict(config: Config, type: Type,
@@ -186,11 +150,93 @@ UCTT_PLUGIN_CONFIG_KEY_ARGS = 'arguments'
 """ new_outputs_from_config will use this Dict key from the output config to decide what arguments to pass to the plugin """
 
 
+def new_plugin_from_config(config: Config, type: Type,
+                           label: str, key: str, instance_id: str = '') -> UCTTPlugin:
+    """ Create a plugin from some config
+
+    This method will interpret some config values as being usable to build plugin
+
+    Using a configerus config object allows us to leverage advanced configerus
+    features such as tree searching, formatting and validation.
+
+    Parameters:
+    -----------
+
+    config (Config) : Used to load and get the plugin configuration
+
+    type (.plugin.Type) : plugin type to create
+
+    label (str) : config label to load to pull plugin configuration. That
+        label is loaded and config is pulled to produce a list of plugins
+
+    key (str) : config key to get a Dict of plugins configurations.
+
+    instance_id (str) : optionally pass an instance_id for the item.
+
+    Returns:
+    --------
+
+    PluginInstances of your type
+
+    Raises:
+    -------
+
+    If you ask for a plugin which has not been registered, then you're going to
+    get a NotImplementedError exception.
+    To make sure that your desired plugin is registered, make sure to import
+    the module that contains the factory method with a decorator.
+
+    """
+    config_plugin = config.load(label)
+    """ loaded configuration for the plugin """
+
+    plugin_id = config_plugin.get(UCTT_PLUGIN_CONFIG_KEY_PLUGINID)
+    if not plugin_id:
+        raise ValueError('Could not find a plugin_id when trying to create a plugin')
+    # if no instance_id was passed, try to load one or just make one up
+    if not instance_id:
+        instance_id = config_plugin.get(UCTT_PLUGIN_CONFIG_KEY_INSTANCEID)
+    if not instance_id:
+        instance_id = '{}-{}'.format(type, plugin_id)
+
+    # If we were given a output config, then create a copy of the config
+    # object and add the passed config as new sources to the copy
+    plugin_dict = config_plugin.get(UCTT_PLUGIN_CONFIG_KEY_CONFIG)
+    if plugin_dict:
+        config = config.copy()
+        # Add a dict source plugin for the passed 'config', giving it source id
+        # just to make identification easier
+        instance_config.add_source(
+            PLUGIN_ID_SOURCE_DICT, '{}-{}-{}'.format(
+                type, plugin_id, instance_id)).set_data(
+            plugin_dict[UCTT_PLUGIN_CONFIG_KEY_CONFIG])
+
+    plugin = new_plugin(
+        config=config,
+        type=type,
+        plugin_id=plugin_id,
+        instance_id=instance_id)
+
+    plugin_args = config_plugin.get(UCTT_PLUGIN_CONFIG_KEY_ARGS)
+    if plugin_args:
+        plugin.arguments(**plugin_args)
+
+    return plugin
+
 def new_plugin_from_dict(config: Config, type: Type,
-                         plugin_dict: Dict[str, Any]) -> UCTTPlugin:
+                         plugin_dict: Dict[str, Any], instance_id: str = '') -> UCTTPlugin:
     """ Create a single plugin from a Dict of information for it
 
     Create a new plugin from a map/dict of settings for the needed parameters.
+    To process this, we convert the dict to a configerus dict source, and build
+    a plugin using new_plugin_from_config, telling it to read from the added
+    config source.
+
+    we use LOADED_KEY_ROOT to tell new_plugin_from_config to load the entire
+    configerus source as the definition of the plugin.
+
+    We use a configerus source so that we can get the configerus fancy features
+    such as tree searching, formatting and validation.
 
     Parameters:
     -----------
@@ -205,43 +251,23 @@ def new_plugin_from_dict(config: Config, type: Type,
 
         @see new_plugin_from_dict for more details.
 
+    instance_id (str) : optionally pass an instance_id for the item.
+
     Return:
     -------
 
     A plugin object (UCTTPlugin)
 
     """
-    if not UCTT_PLUGIN_CONFIG_KEY_PLUGINID in plugin_dict:
-        raise ValueError(
-            "Plugin dict was missing plugin_id : {}".format(plugin_dict))
-    plugin_id = plugin_dict[UCTT_PLUGIN_CONFIG_KEY_PLUGINID]
-    if not UCTT_PLUGIN_CONFIG_KEY_INSTANCEID in plugin_dict:
-        instance_id = '{}-{}'.format(type, plugin_id)
-    else:
-        instance_id = plugin_dict[UCTT_PLUGIN_CONFIG_KEY_INSTANCEID]
+    dict_id = '{}-{}-{}-plugin_dict'.format(type, plugin_id, instance_id)
+    """ unique identifier for this dict as a config label """
 
-    instance_config = config
-    # If we were given a output config, then create a copy of the config
-    # object and add the passed config as new sources to the copy
-    if UCTT_PLUGIN_CONFIG_KEY_CONFIG in plugin_dict:
-        instance_config = config.copy()
-        # Add a dict source plugin for the passed 'config', giving it source id
-        # just to make identification easier
-        instance_config.add_source(
-            PLUGIN_ID_SOURCE_DICT, 'output-{}-{}'.format(
-                plugin_id, instance_id)).set_data(
-            plugin_dict[UCTT_PLUGIN_CONFIG_KEY_CONFIG])
-
-    plugin = new_plugin(
-        config=instance_config,
-        type=type,
-        plugin_id=plugin_id,
-        instance_id=instance_id)
-
-    if UCTT_PLUGIN_CONFIG_KEY_ARGS in plugin_dict:
-        plugin.arguments(**plugin_dict[UCTT_PLUGIN_CONFIG_KEY_ARGS])
-
-    return plugin
+    config = config.copy()
+    instance_config.add_source(
+        PLUGIN_ID_SOURCE_DICT, ).set_data({
+            dict_id: plugin_dict
+        })
+    return new_plugin_from_config(config, type, dict_id, LOADED_KEY_ROOT, instance_id)
 
 
 def new_plugin(config: Config, type: Type, plugin_id: str,
